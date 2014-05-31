ad5fd77b79ef82e7782973068ceb3dbe80305c8a
david <davidf1212@gmail.com>
2014-05-31 17:25:07 -0400

    Main pages added

--- README.md
+++
@@ -1,4 +0,0 @@
-Battlehack 2014
-==========
-Saturday, May 31, 2014
-Matthew Du, Julie Xue, Richard Wong, David Fu
--- js/maincontroller.js
+++ js/maincontroller.js
@@ -1,3 +1,12 @@
 app.controller("MainController", function($scope){
+	$scope.title = "FindMe";
+	$scope.tab0 = "Home";
+	$scope.tab1 = "Lost Item?";
+	$scope.tab2 = "Found Item?";
+	$scope.tab3 = "About Us";
+	$scope.authors = "Matthew Du, Julie Xue, Richard Wong, David Fu";
 	$scope.result = "output";
+});
+
+app.controller("Constants", function($scope){
 });
