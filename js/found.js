f2d839caa219a631269a7f9df7eb34194a8bf0df
matthewdu <du.matthew@gmail.com>
2014-06-02 23:20:10 -0400

    fixed found.html css

--- js/found.js
+++ js/found.js
@@ -1,10 +1,10 @@
 var app = angular.module('Found', []);
 app.controller('FoundController', function($scope) {
-	$scope.title = "FindMe";
+	$scope.projectname = "FindMe";
 	$scope.tab0 = "Home";
-	$scope.tab1 = "Lost Item?";
-	$scope.tab2 = "Found Item?";
-	$scope.tab3 = "About Us";
+	$scope.tab1 = "Lost";
+	$scope.tab2 = "Found";
+	$scope.tab3 = "About";
 	$scope.authors = "Matthew Du, Julie Xue, Richard Wong, David Fu";
 	$scope.result = [];
 
