2763b4f3f42e7cdb446324acef6749f12c2dd04f
matthewdu <du.matthew@gmail.com>
2014-06-02 21:30:32 -0400

    parse.query is working

--- js/found.js
+++ js/found.js
@@ -7,4 +7,16 @@ app.controller('FoundController', function($scope) {
 	$scope.tab3 = "About Us";
 	$scope.authors = "Matthew Du, Julie Xue, Richard Wong, David Fu";
 	$scope.result = "output";
+
+	$scope.search = function() {
+		Parse.initialize("MjJzN3zeFZvB1qehXMZqePWc3JK5wXMdDAElXzOp", "Q5IIgKAt1ETB7YV9inMx2x0xRf8EmF186dkCzLZt");
+		var query = new Parse.Query("LostObject");
+		query.find({
+		  success: function(results) {
+		  	$scope.searchResults = results[0].get('title');
+		  },
+		  error: function(error) {
+		  }
+		});
+	};
 });
